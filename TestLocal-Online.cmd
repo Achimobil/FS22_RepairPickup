7d8ef2a8a681aff93df3a874d030eb0702828c96
Achim Kuntz <achimobil@hotmail.com>
2023-03-10 08:27:10 +0100

    cmds angepasst

--- TestLocal-Online.cmd
+++ TestLocal-Online.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22ModTest\mods\FS22_RepairPickup.zip" -x@FileDefinition.txt
-"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -profile E:\LS22ModTest -skipStartVideos
+"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_RepairPickup.zip" -x@FileDefinition.txt
+"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -profile E:\LS22Profile\LS22ModTest -skipStartVideos
